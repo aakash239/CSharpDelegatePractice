3951c7f451fb77f512dc1e5f7a2d970f0a5edded
AIL-PC-307-Linux <aakash.gupta@argusoft.com>
2026-07-29 13:52:29 +0530

    Created LogDel Delegate

--- DelegateBasicExample/Program.cs
+++ DelegateBasicExample/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace DelegateBasicExample;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DelegateBasicExample;
 
 class Program
 {
+    delegate void LogDel(string text);
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        LogDel logDel = new LogDel(LogTextToScreen);
+        
+        System.Console.WriteLine("Please Enter your name");
+        string name = Console.ReadLine()!;
+        
+        logDel(name);
+    }
+
+    static void LogTextToScreen(string text) 
+    {
+        Console.WriteLine($"{DateTime.Now}: {text}");
     }
 }
