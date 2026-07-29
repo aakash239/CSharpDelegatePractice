e11156bd96b15a17863803faf717145070a81d13
AIL-PC-307-Linux <aakash.gupta@argusoft.com>
2026-07-29 14:25:37 +0530

    Ran the Delegates for both static and non-static methods, MultiCast Delegates, and passing delegates as a Parameter

--- DelegateBasicExample/Program.cs
+++ DelegateBasicExample/Program.cs
@@ -7,16 +7,43 @@ class Program
     delegate void LogDel(string text);
     static void Main(string[] args)
     {
-        LogDel logDel = new LogDel(LogTextToScreen);
+        Log log = new Log();
+        LogDel logTextToScreenDel, logTextToFileDel;
         
-        System.Console.WriteLine("Please Enter your name");
+        logTextToScreenDel = new LogDel(log.LogTextToScreen);
+        logTextToFileDel = new LogDel(log.LogTextToFile);
+        
+        // Multicast Delegates
+        LogDel multiLogDel = logTextToScreenDel + logTextToFileDel;
+
+        Console.WriteLine("Please Enter your name");
         string name = Console.ReadLine()!;
-        
-        logDel(name);
+
+        // implementing delegate passed as a parameter
+        LogText(multiLogDel, name);
     }
 
-    static void LogTextToScreen(string text) 
+    // delegate passed as a functional parameter
+    static void LogText (LogDel logDel, string text)
+    {
+        logDel(text);
+    }
+}
+
+public class Log
+{
+    public void LogTextToScreen(string text) 
     {
         Console.WriteLine($"{DateTime.Now}: {text}");
+    }
+
+    public void LogTextToFile(string text)
+    {
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.txt"), true))
+        {
+            sw.WriteLine($"{DateTime.Now}: {text}");
+        }
+
+        Console.WriteLine("Written on log file.");
     }
 }
