5f264050ec168eed29b039346764e42d1bc9ff64
HomeLaptop <aakash.gupta@argusoft.com>
2026-07-30 23:02:26 +0530

    Created Basic Arithmetic Example Successfully

--- DelegateArithmeticExample/Program.cs
+++ DelegateArithmeticExample/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace DelegateArithmeticExample;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        BasicCalculator.MathOperator op = BasicCalculator.Add;
+        Console.WriteLine($"Add 3 and 4: {op(3,4)}");
+
+        op = BasicCalculator.Multiply;
+        Console.WriteLine($"Multiply 3 and 4: {op(3,4)}");
+    }
+}
+
+internal class BasicCalculator
+{
+    public delegate int MathOperator(int a, int b);
+    
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
     }
 }
