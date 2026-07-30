1ef3c8c768142d8b9c9cb30f2696432d660110dc
HomeLaptop <aakash.gupta@argusoft.com>
2026-07-31 00:09:48 +0530

    Delegate Chaining in Return type non-void

--- DelegateArithmeticExample/Program.cs
+++ DelegateArithmeticExample/Program.cs
@@ -9,6 +9,18 @@ internal class Program
 
         op = BasicCalculator.Multiply;
         Console.WriteLine($"Multiply 3 and 4: {op(3,4)}");
+
+        op = BasicCalculator.Add; 
+        op += BasicCalculator.Multiply;
+
+        // If a multicast delegate has both Add and Multiply attached, and it has a non-void return type, what does invoking it actually return?
+        Console.WriteLine($"Invoked both methods only last method is returned and others are discarded: {op(3,4)}");
+
+        foreach (var d in op.GetInvocationList())
+        {
+            var typedDelegate = (BasicCalculator.MathOperator)d;
+            Console.WriteLine($"{d.Method.Name}(3,4) = {typedDelegate(3,4)}");
+        }       
     }
 }
 
