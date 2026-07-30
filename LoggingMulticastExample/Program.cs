e40c929579020d159b42e38e230c6df051a9e4c4
HomeLaptop <aakash.gupta@argusoft.com>
2026-07-30 23:57:38 +0530

    Completed Logger Example

--- LoggingMulticastExample/Program.cs
+++ LoggingMulticastExample/Program.cs
@@ -1,9 +1,41 @@
 ﻿namespace LoggingMulticastExample;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Logger logger = new Logger("Log");
+        Logger.Notify? notify = logger.LogToFile;
+
+        notify += logger.LogToConsole;
+        notify += logger.SendAlert;
+
+        notify("Added logging");        
+        notify -= logger.SendAlert; //Possible null reference assignment. shows this warning tell how to fix
+
+        notify?.Invoke("removed Alerts");
+    }
+}
+
+public class Logger
+{
+    public string messageInternal;
+    public delegate void Notify(string message);
+    public Logger(string messageGot)
+    {
+        this.messageInternal = messageGot;
+    }
+
+    public void LogToConsole(string message)
+    {
+        Console.WriteLine($"Console: {message}, {messageInternal}");
+    }
+    public void LogToFile(string message)
+    {
+        Console.WriteLine($"File: {message}, {messageInternal}");
+    }
+    public void SendAlert(string message)
+    {
+        Console.WriteLine($"Alert: {message}, {messageInternal}");
     }
 }
