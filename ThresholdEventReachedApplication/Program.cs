43e118b74c9e7dc28d04bed17bee566638b86255
AIL-PC-307-Linux <aakash.gupta@argusoft.com>
2026-07-30 15:31:06 +0530

    Counter Application using Events Exercise

--- ThresholdEventReachedApplication/Program.cs
+++ ThresholdEventReachedApplication/Program.cs
@@ -1,9 +1,49 @@
-﻿namespace ThresholdEventReachedApplication;
+﻿using System.Net;
 
-class Program
+namespace ThresholdEventReachedApplication;
+
+class EventNoData
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int threshold = 10;
+        Counter c = new(threshold);
+        c.ThresholdReached += c_thresholdReached; 
+        
+        Console.WriteLine("press 'a' key to increase total");
+
+        while (Console.ReadKey(true).KeyChar == 'a')
+        {
+            Console.WriteLine("adding one");
+            c.AddOne();
+        }
     }
+
+    static void c_thresholdReached(Object? sender, EventArgs e)
+    {
+        Console.WriteLine("The threshold was reached.");
+        Environment.Exit(0);
+    }
+}
+
+class Counter (int passedThreshold)
+{
+    public readonly int _threshold = passedThreshold;
+    private int _total;
+
+    public event EventHandler? ThresholdReached;
+    public void AddOne()
+    {
+        _total += 1;
+        if(_total >= _threshold)
+        {
+            OnThresholdReached(EventArgs.Empty);
+        }
+    }
+
+    protected virtual void OnThresholdReached(EventArgs e)
+    {
+        ThresholdReached?.Invoke(this, e);
+    }
+
 }
