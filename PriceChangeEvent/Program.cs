6ff17d6620b4b822757b041e1096b54e45f39015
AIL-PC-307-Linux <aakash.gupta@argusoft.com>
2026-07-31 11:35:16 +0530

    Completed the priceChange Event multicast

--- PriceChangeEvent/Program.cs
+++ PriceChangeEvent/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Stock stock = new();
+        stock.priceChanged += ReactToChangeInPrice;
+        stock.priceChanged += ReactToChangeInPrice2;  
+
+        stock.SetPrice(150);
+        stock.SetPrice(200);
+    }
+
+    private static void ReactToChangeInPrice(decimal oldPrice, decimal newPrice)
+    {
+        Console.WriteLine($"Reacted to change1 in oldPrice{oldPrice} , NewPrice: {newPrice}!!");
+    }
+     private static void ReactToChangeInPrice2(decimal oldPrice, decimal newPrice)
+    {
+        Console.WriteLine($"Reacted to change2 in oldPrice{oldPrice} , NewPrice: {newPrice}!!");
+    }
+
+}
+
+class Stock
+{
+    public delegate void PriceChangeHandler(decimal oldPrice, decimal newPrice);
+
+    public event PriceChangeHandler? priceChanged;
+
+    private decimal price = 100;
+
+    public void SetPrice(decimal newPrice)
+    {
+        decimal oldPrice = price;
+        price = newPrice;
+        priceChanged?.Invoke(oldPrice, newPrice);
     }
 }
